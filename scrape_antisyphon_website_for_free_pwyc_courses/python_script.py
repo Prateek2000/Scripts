3264ca292981215688095ae8e714d105cbdbb2d6
Prateek2000 <sharma.pratik2000@gmail.com>
2024-02-05 01:22:29 +0530

    Improved comments. Output shows only course names instead of html

--- scrape_antisyphon_website_for_free_pwyc_courses/python_script.py
+++ scrape_antisyphon_website_for_free_pwyc_courses/python_script.py
@@ -4,10 +4,10 @@
 from bs4 import BeautifulSoup
 import requests
 soup =  BeautifulSoup(requests.get('https://www.antisyphontraining.com/training-calendar/list/?tribe_eventcategory%5B0%5D=125').text, 'html.parser') #125 is possibly for "Pay what you can" events
-#xpath magic
+#xpath magic (find divs containing the events)
 events = soup.find_all('div', class_='tribe-events-calendar-list__event-wrapper')
 for event in events:
-    #more xpath magic
+    #more xpath magic (filter spans in each event that contain the price, the free events have no such elements, so we compare with None)
     if(event.find("span", class_="tribe-events-c-small-cta__price")==None):
-        #even more xpath magic
-        print(event.find_all("a", class_='tribe-events-calendar-list__event-title-link tribe-common-anchor-thin'))
+        #even more xpath magic (print the name of the event)
+        print(event.find("a", class_='tribe-events-calendar-list__event-title-link tribe-common-anchor-thin').text.strip())
